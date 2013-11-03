92b088cda660e0cb0e499e8842c074bf654fdac4
keith.stabbert <keith.stabbert@yahoo.com>
2013-11-02 17:30:45 -0700

    It works

--- lms/app/controllers/EmployeeController.java
+++ lms/app/controllers/EmployeeController.java
@@ -14,50 +14,59 @@ import views.html.*;
 public class EmployeeController extends Controller {
   
 
-    @Transactional
-    public static Result employee(Integer id){
-        Form<EmployeePOJO> employeeForm = form(EmployeePOJO.class).fill(
-        			EmployeeHelper.getEmployeePOJO(id)
-        );
-        return ok(
-            views.html.employee.employee.render(employeeForm)
-        );
-    }
-    
+//    @Transactional
+//    public static Result employee(Integer id){
+//        Form<EmployeePOJO> employeeForm = form(EmployeePOJO.class).fill(
+//        			EmployeeHelper.getEmployeePOJO(id)
+//        );
+//        return ok(
+//            views.html.employee.employee.render("V", employeeForm)
+//        );
+//    }
+	@Transactional
+	public static Result editEmployee(Integer id){
+	    Form<EmployeePOJO> employeeForm = form(EmployeePOJO.class).fill(
+	      			EmployeeHelper.getEmployeePOJO(id)
+	    );
+	    return ok(
+	        views.html.employee.employee.render("E", employeeForm)
+	    );
+	}
+	
     @Transactional(readOnly=true)
     public static Result employees(){
         return ok(
         	views.html.employee.employees.render(EmployeeHelper.getAll())
         );
     }
-//    
-//    @Transactional
-//    public static Result newEmployee(){
-//        Form<Employee> employeeForm = form(Employee.class);
-//        return ok(
-//            views.html.employee.createEmployee.render(employeeForm)
-//        );
-//    }
-//    
-//    @Transactional
-//    public static Result save() {
-//        Form<Employee> employeeForm = form(Employee.class).bindFromRequest();
-//        if(employeeForm.hasErrors()) {
-//            return badRequest(views.html.employee.employee.render(employeeForm));
-//        }
-//        employeeForm.get().save();
-//        return redirect(routes.EmployeeController.employees());
-//    }
-//    
-//    @Transactional
-//    public static Result update(Integer employeeNumber){
+    
+    @Transactional
+    public static Result newEmployee(){
+        Form<EmployeePOJO> employeeForm = form(EmployeePOJO.class);
+        return ok(
+            views.html.employee.createEmployee.render(employeeForm)
+        );
+    }
+    
+    @Transactional
+    public static Result save() {
+        Form<EmployeePOJO> employeeForm = form(EmployeePOJO.class).bindFromRequest();
+        if(employeeForm.hasErrors()) {
+            return badRequest(views.html.employee.createEmployee.render(employeeForm));
+        }
+        EmployeeHelper.save(employeeForm.get());
+        return redirect(routes.EmployeeController.employees());
+    }
+    
+    @Transactional
+    public static Result update(Integer employeeNumber){
 //        Form<Employee> employeeForm = form(Employee.class).bindFromRequest();
 //        if(employeeForm.hasErrors()) {
 //            return badRequest(views.html.employee.employee.render(employeeForm));
 //        }
 //        employeeForm.get().update(employeeNumber);
-//        return redirect(routes.EmployeeController.employees());
-//    }
+        return redirect(routes.EmployeeController.employees());
+    }
 //    
 //    public static Result deleteEmployee(Integer id){
 //    	return TODO;
