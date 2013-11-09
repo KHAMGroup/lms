96fa9f74fa60d9e5b9c70b407923e6d33ff92d40
keith.stabbert <keith.stabbert@yahoo.com>
2013-11-09 15:44:25 -0800

    Getting rid of received_by's default value.

--- lms/conf/evolutions/default/2.sql
+++ lms/conf/evolutions/default/2.sql
@@ -10,13 +10,13 @@ INSERT INTO CLIENT(last,first,city,zip, office_phone,phone_report_OK, email_repo
 VALUES('goodman','saul','Riverside','92005','9098675309',1,1);
 
 
-INSERT INTO CASES(clt_no, case_number, subject_lastname, subject_firstname,  received_by, sample_type, email_results_OK) 
-VALUES((SELECT c.client_id FROM CLIENT AS c WHERE c.last = 'goodman'), '007','Heisenberg', 'Walter', (SELECT employee_number FROM EMPLOYEES WHERE username = 'labman'), 
+INSERT INTO CASES(clt_no, case_number, subject_lastname, subject_firstname,  received_date, received_by, sample_type, email_results_OK) 
+VALUES((SELECT c.client_id FROM CLIENT AS c WHERE c.last = 'goodman'), '007','Heisenberg', 'Walter', CURDATE(), (SELECT employee_number FROM EMPLOYEES WHERE username = 'labman'), 
 'blood',  1); 
 
 
 INSERT INTO CASES(clt_no, case_number, subject_lastname, subject_firstname, received_date, received_by, sample_type, email_results_OK) 
-VALUES((SELECT c.client_id FROM CLIENT AS c WHERE c.last = 'goodman'), '008','Heisenberg', 'Walter', NOW(), (SELECT employee_number FROM EMPLOYEES WHERE username = 'labman'), 
+VALUES((SELECT c.client_id FROM CLIENT AS c WHERE c.last = 'goodman'), '008','Heisenberg', 'Walter', CURDATE(), (SELECT employee_number FROM EMPLOYEES WHERE username = 'labman'), 
 'blood', 1); 
 
 INSERT INTO TEST(test_number, test_name, test_type)
