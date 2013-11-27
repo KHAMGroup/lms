f4e5a5523d702b392c7b5d26fd10b090adabd739
keith.stabbert <keith.stabbert@yahoo.com>
2013-11-27 13:53:22 -0800

    Started Create Report

--- lms/app/controllers/ReportController.java
+++ lms/app/controllers/ReportController.java
@@ -4,6 +4,8 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+import controllers.MainController.SearchQuery;
+
 import play.*;
 import play.mvc.*;
 import static play.data.Form.*;
@@ -14,5 +16,16 @@ import models.helpers.ReportHelper;
 
 @Security.Authenticated(Avocado.class)
 public class ReportController extends Controller {
-
+	
+	@Transactional
+	public static Result createReport(String caseNumber) {
+		CaseEntityObject theCase = CaseEntityObject.findByCaseNumber(caseNumber);
+		if(theCase == null){
+			return redirect(routes.MainController.searchResults("Case", caseNumber));
+		}
+		
+		ReportPOJO report = new ReportPOJO();
+		Form<ReportPOJO> reportForm = form(ReportPOJO.class).fill(report);
+		return ok(views.html.report.edit_report.render(theCase, reportForm));
+	}
 }
